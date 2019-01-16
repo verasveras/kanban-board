6aa2e47ca9d4354b88eac4ad9a649389268e60c0
Lisa Veras <lisaveras@gmail.com>
2019-01-15 20:35:53 -0500

    styled columns added

---
+++ src/js/components/column/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Connect } from 'react-redux';
+
+// props :
+// name
+// tasks :
+// handleCreateTask
+// show createButton
+
+export default class Column extends React.Component {
+  render() {
+    const buttonColor = this.props.name === 'Backlog' ? 'blue' : 'purple';
+    return (
+      <div className="column">
+        <div className="column__title">{this.props.name}</div>
+        {this.props.showCreateButton && (
+          <div
+            className={`create-button create-button__${buttonColor}`}
+            onClick={this.props.handleCreateTask}
+          >
+            Create Task
+          </div>
+        )}
+      </div>
+    );
+  }
+}

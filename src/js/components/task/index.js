fe9192762e5553422ff9011ad32ba41ac71a9c44
Lisa Veras <lisaveras@gmail.com>
2019-01-17 19:25:46 -0500

    styling for completed tasks

--- src/js/components/task/index.js
+++ src/js/components/task/index.js
@@ -4,14 +4,17 @@ import Buttons from './buttons';
 export default class Task extends React.Component {
   render() {
     const { task } = this.props;
+    const isCompleted = this.props.type === 'Completed';
     const description = task.description ? task.description : 'N/A';
     const dueDateText = `Due ${task.dueDate}`;
 
     return (
-      <div className="task">
+      <div className={`task ${isCompleted ? 'task-completed' : ''}`}>
         <div className="task__title">{task.title}</div>
         <div className="task__description">{description}</div>
-        <div className="task__date">
+        <div
+          className={`task__date ${isCompleted ? 'task__date-completed' : ''}`}
+        >
           <i className="fa fa-calendar-day" />
           {dueDateText}
         </div>
